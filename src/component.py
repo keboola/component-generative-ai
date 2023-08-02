0f0825890ff9656d30e0d02121d869fd8188d1fb
SgtMarmite <d.novotny31415@gmail.com>
2023-08-02 09:31:09 +0200

    add openai-app files

--- src/component.py
+++ src/component.py
@@ -3,20 +3,42 @@ Template Component main class.
 
 """
 import csv
+import dataclasses
 import logging
-from datetime import datetime
+import time
+from typing import List
+import json
+import os
+import backoff
 
-from keboola.component.base import ComponentBase
+import openai
+import pystache as pystache
+from keboola.component.base import ComponentBase, sync_action
+from keboola.component.sync_actions import ValidationResult, MessageType
+from keboola.component.dao import TableDefinition
 from keboola.component.exceptions import UserException
+from keboola.utils.helpers import comma_separated_values_to_list
+
+from configuration import Configuration
 
 # configuration variables
+RESULT_COLUMN_NAME = 'result_value'
 KEY_API_TOKEN = '#api_token'
-KEY_PRINT_HELLO = 'print_hello'
+KEY_SLEEP = 'sleep'
+KEY_PROMPT = 'prompt'
+KEY_DESTINATION = 'destination'
+KEY_STORE_RESULTS_ON_FAILURE = 'store_results_on_failure'
 
 # list of mandatory parameters => if some is missing,
 # component will fail with readable message on initialization.
-REQUIRED_PARAMETERS = [KEY_PRINT_HELLO]
+REQUIRED_PARAMETERS = [KEY_API_TOKEN, KEY_PROMPT, KEY_DESTINATION]
 REQUIRED_IMAGE_PARS = []
+
+CHAT_COMPLETION_MODELS = ["gpt-4", "gpt-4-0314", "gpt-4-32k", "gpt-4-32k-0314", "gpt-3.5-turbo", "gpt-3.5-turbo-0301"]
+
+
+def on_giveup(details: dict):
+    raise UserException(details.get("exception"))
 
 
 class Component(ComponentBase):
@@ -32,45 +54,239 @@ class Component(ComponentBase):
 
     def __init__(self):
         super().__init__()
+        self.model_options = None
+        self.inference_function = None
+        self.store_results_on_failure = None
+        self.input_keys = None
+        self.sleep_time = None
+        self.queue_v2 = None
+        self.model = None
+        self._configuration = None
+        self.failed_requests = 0
 
     def run(self):
         """
         Main execution code
         """
+        self.init_configuration()
 
-        # ####### EXAMPLE TO REMOVE
-        # check for missing configuration parameters
-        self.validate_configuration_parameters(REQUIRED_PARAMETERS)
-        self.validate_image_parameters(REQUIRED_IMAGE_PARS)
-        params = self.configuration.parameters
-        # Access parameters in data/config.json
-        if params.get(KEY_PRINT_HELLO):
-            logging.info("Hello World")
+        input_table, out_table = self.prepare_tables()
 
-        # get last state data/in/state.json from previous run
-        previous_state = self.get_state_file()
-        logging.info(previous_state.get('some_state_parameter'))
+        logging.info('Querying OpenAI.')
+        with open(input_table.full_path, 'r') as input_file:
+            reader = csv.DictReader(input_file)
+            with open(out_table.full_path, 'w+') as out_file:
+                writer = csv.DictWriter(out_file, fieldnames=out_table.primary_key + [RESULT_COLUMN_NAME])
+                writer.writeheader()
+                for row in reader:
+                    prompt = self._build_prompt(self.input_keys, row)
+                    result = self.inference_function(self.model, prompt, **self.model_options)
 
-        # Create output table (Tabledefinition - just metadata)
-        table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
+                    if result:
+                        writer.writerow(self._build_output_row(out_table.primary_key, row, result))
+                    time.sleep(self.sleep_time)
 
-        # get file path of the table (data/out/tables/Features.csv)
-        out_table_path = table.full_path
-        logging.info(out_table_path)
+        self.write_manifest(out_table)
 
-        # DO whatever and save into out_table_path
-        with open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
-            writer = csv.DictWriter(out_file, fieldnames=['timestamp'])
-            writer.writeheader()
-            writer.writerow({"timestamp": datetime.now().isoformat()})
+        if self.failed_requests > 0:
+            if self.store_results_on_failure:
+                if self.queue_v2:
+                    self.add_flag_to_manifest()
+                raise UserException(f"Component has failed to process {self.failed_requests} records.")
+        else:
+            logging.info("All rows processed.")
 
-        # Save table manifest (output.csv.manifest) from the tabledefinition
-        self.write_manifest(table)
+    def init_configuration(self):
+        self.validate_configuration_parameters(Configuration.get_dataclass_required_parameters())
+        self._configuration: Configuration = Configuration.load_from_dict(self.configuration.parameters)
 
-        # Write new state - will be available next run
-        self.write_state_file({"some_state_parameter": "value"})
+        self.sleep_time = self._configuration.sleep
+        self.input_keys = self._get_input_keys(self._configuration.prompt)
 
-        # ####### EXAMPLE TO REMOVE END
+        self.queue_v2 = False
+        self.store_results_on_failure = self._configuration.destination.store_results_on_failure
+        if self.store_results_on_failure:
+            self.queue_v2 = 'queuev2' in os.environ.get('KBC_PROJECT_FEATURE_GATES', '')
+            if self.queue_v2:
+                logging.info("Component will try to save results even if some queries fail.")
+            else:
+                logging.warning("Running on old queue, results cannot be stored on failure.")
+
+        openai.api_key = self._configuration.pswd_api_token
+
+        self.model = self._configuration.custom_model or self._configuration.predefined_model
+        logging.info(f"The component is using the model : {self.model}")
+
+        self.model_options = dataclasses.asdict(self._configuration.additional_options)
+
+        self.inference_function = self.get_inference_function(self.model)
+
+    def prepare_tables(self, include_output_table: bool = True):
+        input_table = self._get_input_table()
+        if missing_keys := [key for key in self.input_keys if key not in input_table.columns]:
+            raise UserException(f'The columns "{missing_keys}" need to be present in the input data!')
+
+        out_table = self._build_out_table()
+
+        if missing_keys := [t for t in out_table.primary_key if t not in input_table.columns]:
+            raise UserException(f'Some specified primary keys are not in the input table: {missing_keys}')
+
+        return input_table, out_table
+
+    @backoff.on_exception(backoff.expo,
+                          (openai.error.RateLimitError, openai.error.APIError, openai.error.ServiceUnavailableError),
+                          max_tries=3, on_giveup=on_giveup)
+    def get_completion_result(self, model, prompt, **model_options):
+        try:
+            response = openai.Completion.create(
+                model=model,
+                prompt=prompt,
+                **model_options
+            )
+        except openai.error.InvalidRequestError as e:
+            logging.error(f"Invalid Request Error: {e}")
+            self.failed_requests += 1
+            return None
+        except openai.error.AuthenticationError as e:
+            raise UserException("Your OpenAI API key is invalid") from e
+        except openai.error.APIConnectionError as e:
+            raise UserException(f"API connection Error: {e}") from e
+
+        return response.choices[0].text
+
+    @backoff.on_exception(backoff.expo,
+                          (openai.error.RateLimitError, openai.error.APIError, openai.error.ServiceUnavailableError),
+                          max_tries=3, on_giveup=on_giveup)
+    def get_chat_completion_result(self, model, prompt, **model_options):
+        try:
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=[{"role": "user", "content": prompt}],
+                **model_options
+            )
+        except openai.error.InvalidRequestError as e:
+            logging.error(f"Invalid Request Error: {e}")
+            self.failed_requests += 1
+            return None
+        except openai.error.AuthenticationError as e:
+            raise UserException("Your OpenAI API key is invalid") from e
+        except openai.error.APIConnectionError as e:
+            raise UserException(f"API connection Error: {e}") from e
+
+        return response.choices[0].get("message", {}).get("content")
+
+    def get_inference_function(self, model_name: str):
+        if model_name in CHAT_COMPLETION_MODELS:
+            return self.get_chat_completion_result
+        return self.get_completion_result
+
+    @staticmethod
+    def _get_sleep(params):
+        sleep_time = float(params.get(KEY_SLEEP, 0))
+        logging.info(f"Sleep time set to {sleep_time} seconds." if sleep_time else "Sleep time not set.")
+        return sleep_time
+
+    @staticmethod
+    def _build_output_row(primary_key: List[str], input_row: dict, result: str):
+        row = dict()
+        for k in primary_key:
+            row[k] = input_row[k]
+        row['result_value'] = result.strip()
+        return row
+
+    def _build_out_table(self) -> TableDefinition:
+        destination_config = self.configuration.parameters['destination']
+        out_table_name = f"{destination_config['output_table_name']}.csv"
+
+        # backward compatibility with the old string element
+        if destination_config.get('primary_keys', None):
+            primary_key = comma_separated_values_to_list(destination_config.get('primary_keys', None))
+        else:
+            primary_key = destination_config.get('primary_keys_array', [])
+
+        incremental_load = destination_config.get('incremental_load', False)
+        return self.create_out_table_definition(out_table_name, primary_key=primary_key,
+                                                incremental=incremental_load)
+
+    @staticmethod
+    def _get_input_keys(prompt: str):
+        template = pystache.parse(prompt, delimiters=('[[', ']]'))
+        keys = [token.key for token in template._parse_tree if hasattr(token, "key")]  # noqa
+        if len(keys) < 1:
+            raise UserException('You must provide at least one input placeholder. 0 were found.')
+        return keys
+
+    def _build_prompt(self, input_keys: List[str], row: dict):
+        prompt = self.configuration.parameters['prompt']
+        for input_key in input_keys:
+            prompt = prompt.replace('[[' + input_key + ']]', row[input_key])
+        return prompt
+
+    def _get_input_table(self) -> TableDefinition:
+        if not self.get_input_tables_definitions():
+            raise UserException("No input table specified. Please provide one input table in the input mapping!")
+        return self.get_input_tables_definitions()[0]
+
+    def add_flag_to_manifest(self):
+        manifests = [x for x in os.listdir(self.tables_out_path) if x.endswith('.manifest')]
+        if manifests:
+            for filename in manifests:
+                path = os.path.join(self.tables_out_path, filename)
+
+                with open(path, 'r') as f:
+                    data = json.load(f)
+                    data['write_always'] = True
+
+                with open(path, 'w') as f:
+                    json.dump(data, f)
+
+    @sync_action('list_pkeys')
+    def list_table_columns(self):
+        """
+        Sync action to fill the UI element of primary keys.
+
+        Returns:
+
+        """
+        input_table = self._get_input_table()
+        return [{"value": c, "label": c} for c in input_table.columns]
+
+    @sync_action('testQuery')
+    def test_query(self) -> ValidationResult:
+        self.init_configuration()
+        input_table, out_table = self.prepare_tables()
+
+        results = []
+        with open(input_table.full_path, 'r') as input_file:
+            reader = csv.DictReader(input_file)
+
+            row_count = 0
+            for row in reader:
+                prompt = self._build_prompt(self.input_keys, row)
+                result = self.inference_function(self.model, prompt, **self.model_options)
+
+                if result:
+                    results.append(self._build_output_row(out_table.primary_key, row, result))
+                time.sleep(self.sleep_time)
+
+                row_count += 1
+                if row_count == 3:
+                    break  # Stop processing after three rows
+
+        markdown = self.create_markdown_table(results)
+        return ValidationResult(markdown, MessageType.SUCCESS)
+
+    @staticmethod
+    def create_markdown_table(data):
+        if not data:
+            return ""
+        headers = list(data[0].keys())
+        table = "| " + " | ".join(headers) + " |\n"
+        table += "| " + " | ".join(["---"] * len(headers)) + " |\n"
+        for row in data:
+            row_values = [str(row[header]) for header in headers]
+            table += "| " + " | ".join(row_values) + " |\n"
+        return table
 
 
 """
---
+++ src/configuration.py
@@ -0,0 +1,75 @@
+import dataclasses
+import json
+from dataclasses import dataclass
+from typing import List
+
+import dataconf
+
+
+class ConfigurationBase:
+    @staticmethod
+    def _convert_private_value(value: str):
+        return value.replace('"#', '"pswd_')
+
+    @staticmethod
+    def _convert_private_value_inv(value: str):
+        if value and value.startswith("pswd_"):
+            return value.replace("pswd_", "#", 1)
+        else:
+            return value
+
+    @classmethod
+    def load_from_dict(cls, configuration: dict):
+        """
+        Initialize the configuration dataclass object from dictionary.
+        Args:
+            configuration: Dictionary loaded from json configuration.
+
+        Returns:
+
+        """
+        json_conf = json.dumps(configuration)
+        json_conf = ConfigurationBase._convert_private_value(json_conf)
+        return dataconf.loads(json_conf, cls, ignore_unexpected=True)
+
+    @classmethod
+    def get_dataclass_required_parameters(cls) -> List[str]:
+        """
+        Return list of required parameters based on the dataclass definition (no default value)
+        Returns: List[str]
+
+        """
+        return [cls._convert_private_value_inv(f.name)
+                for f in dataclasses.fields(cls)
+                if f.default == dataclasses.MISSING
+                and f.default_factory == dataclasses.MISSING]
+
+
+@dataclass
+class Destination(ConfigurationBase):
+    incremental_load: bool
+    output_table_name: str
+    primary_keys_array: list[str]
+    store_results_on_failure: bool
+
+
+@dataclass
+class AdditionalOptions(ConfigurationBase):
+    top_p: float
+    max_tokens: int
+    temperature: float
+    presence_penalty: float
+    frequency_penalty: float
+
+
+@dataclass
+class Configuration(ConfigurationBase):
+    pswd_api_token: str
+    model_type: str
+    prompt: str
+    sleep: float
+    destination: Destination
+    additional_options: AdditionalOptions
+    debug: bool = False
+    predefined_model: str = ""
+    custom_model: str = ""
