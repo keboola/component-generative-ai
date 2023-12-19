310c1ce7c6bc73b6c73752c159ccf6fb7aae1a22
SgtMarmite <d.novotny31415@gmail.com>
2023-12-19 13:32:54 +0100

    reduce retry count

--- src/client/googleai_client.py
+++ src/client/googleai_client.py
@@ -17,7 +17,7 @@ class GoogleAIClient(CommonClient):
         if not self.model:
             self.model = genai.GenerativeModel(model_name=model_name)
 
-        max_retries = 10
+        max_retries = 4
         current_retry = 0
 
         while current_retry < max_retries:
