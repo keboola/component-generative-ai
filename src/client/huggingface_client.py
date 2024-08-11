52215a6a85d75812c2b31371f38906c944117304
SgtMarmite <dominik.novotny@keboola.com>
2024-08-11 13:18:57 +0200

    use asyncClient

--- src/client/huggingface_client.py
+++ src/client/huggingface_client.py
@@ -1,70 +1,82 @@
-import aiohttp
 import backoff
 import logging
 from typing import Optional, Tuple
-import asyncio
 from .base import CommonClient, AIClientException
+from httpx import HTTPStatusError
 
-SUPPORTED_MODELS = ['meta-llama/Meta-Llama-3-8B', 'mistralai/Mixtral-8x7B-Instruct-v0.1', 'microsoft/phi-2']
+
+from keboola.http_client import AsyncHttpClient
+
+SUPPORTED_MODELS = {
+    "mistral-7b-instruct-v0-1-fep": "https://qwiarbdt0vaeh0cb.us-east-1.aws.endpoints.huggingface.cloud",
+    "gemma-7b-it-voa": "https://yi9ctuuxjnmgtjkc.us-east-1.aws.endpoints.huggingface.cloud",
+    "meta-llama-3-8b-noz": "https://nbd6y80gxtows9b3.us-east-1.aws.endpoints.huggingface.cloud"
+}
 
 
-def giveup_handler(details):
-    raise AIClientException(f"All retries failed after {details['tries']} attempts over {details['elapsed']:0.2f} "
-                            f"seconds. Last exception: {details['exception']}")
+class IsSleepingException(Exception):
+    pass
 
 
 class HuggingfaceClient(CommonClient):
     def __init__(self, api_key):
         self.client = None
         self.headers = {
+            'Accept': 'application/json',
             'Authorization': f'Bearer {api_key}',
             'Content-Type': 'application/json'
         }
-        self.semaphore = asyncio.Semaphore(1)
 
-    @backoff.on_exception(backoff.expo, aiohttp.ClientResponseError, max_time=60, giveup=giveup_handler)
     async def infer(self, model_name: str, prompt: str, **model_options) -> Tuple[Optional[str], Optional[int]]:
-        async with self.semaphore:
-            if "max_tokens" in model_options:
-                model_options["max_new_tokens"] = model_options["max_tokens"]
-                del model_options["max_tokens"]
+        if model_name not in SUPPORTED_MODELS:
+            raise AIClientException(f"Model {model_name} is not supported. "
+                                    f"Supported models: {list(SUPPORTED_MODELS.keys())}")
 
-            if "presence_penalty" in model_options:
-                if model_options["presence_penalty"] <= 0:
-                    logging.warning("presence_penalty (repetition_penalty) should be greater than 0. "
-                                    "This parameter will be ignored.")
-                    del model_options["presence_penalty"]
+        if not self.client:
+            self.client = AsyncHttpClient(base_url=SUPPORTED_MODELS[model_name], default_headers=self.headers,
+                                          max_requests_per_second=1)
 
-                model_options["repetition_penalty"] = model_options["presence_penalty"]
-                del model_options["presence_penalty"]
+        max_new_tokens = model_options.get("max_tokens")
+        model_options = {"max_new_tokens": max_new_tokens} if max_new_tokens else {}
 
-            logging.debug(f"Using model options: {model_options}")
-            content = await self.generate_text(prompt=prompt, model_name=model_name, **model_options)
-            logging.debug(f"Response: {content}")
+        logging.debug(f"Prompt: {prompt}")
+        logging.debug(f"Using model options: {model_options}")
 
-            token_usage = await self.get_total_tokens(model_name, prompt, content)
+        content = await self.generate_text(prompt=prompt, model_name=model_name, **model_options)
+        logging.debug(f"Response: {content}")
 
-            return content, token_usage
+        token_usage = await self.get_total_tokens(model_name, prompt, content)
 
+        return content, token_usage
+
+    @backoff.on_exception(backoff.expo, IsSleepingException, max_time=500)
     async def generate_text(self, prompt: str, model_name: str, **model_options) -> str:
-        endpoint = f'https://api-inference.huggingface.co/models/{model_name}'
+        endpoint = SUPPORTED_MODELS.get(model_name)
+
         data = {
             'inputs': prompt,
             'parameters': model_options
         }
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post(endpoint, headers=self.headers, json=data) as response:
-                result = await response.json()
-                try:
-                    response.raise_for_status()
-                    return result[0]['generated_text']
-                except Exception as e:
-                    raise e
+        try:
+            result = await self.client.post(endpoint, json=data)
+        except HTTPStatusError as e:
+            if self._is_asleep(e.response.status_code):
+                logging.warning("The model is currently sleeping. The component will now wait "
+                                "for the model to wake up.")
+                raise IsSleepingException()
+            else:
+                raise AIClientException(f"HTTP error occurred: {e.response.status_code} {e.response.reason_phrase}")
+
+        return result[0]['generated_text']
+
+    @staticmethod
+    def _is_asleep(status_code: int) -> bool:
+        return status_code == 503
 
     @staticmethod
     async def get_total_tokens(model: str, prompt: str, response: str = "") -> int:
         return 0
 
     async def list_models(self) -> list:
-        return SUPPORTED_MODELS
+        return list(SUPPORTED_MODELS.keys())
