75e02ac42ea19831fe5834b3021bb80debe7c39a
SgtMarmite <dominik.novotny@keboola.com>
2024-08-12 10:22:20 +0200

    cleanup configuration.py

--- src/configuration.py
+++ src/configuration.py
@@ -66,7 +66,6 @@ class AdditionalOptions(ConfigurationBase):
 class Authentication(ConfigurationBase):
     service: str
     pswd_api_token: str
-    api_token: str = ""  # stack parameter
     api_base: str = ""
     deployment_id: str = ""
     api_version: str = ""
